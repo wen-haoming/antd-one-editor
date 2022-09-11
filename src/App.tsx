277f55832b4848137b9eef0019a9e9d0bc393083
haoming <435203093@qq.com>
2022-09-11 11:28:17 +0800

    feat: 修改文件夹大小写问题

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { ConfigProvider } from 'antd';
 import zhCN from 'antd/es/locale/zh_CN';
 import 'antd/dist/antd.compact.less'; // 引入官方提供的紧凑 less 样式入口文件
-import Layout from './LayoutPage';
+import Layout from './Layout';
 
 function App() {
   return (
