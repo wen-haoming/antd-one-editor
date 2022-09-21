77aff8e74b78c779c6e5c2980478c43192e773da
haoming <435203093@qq.com>
2022-09-22 01:31:07 +0800

    fix: 修复类型

--- src/components/FormRender/components/TextArea/index.tsx
+++ src/components/FormRender/components/TextArea/index.tsx
@@ -1,10 +1,9 @@
-import type { InputProps } from 'antd';
+// import type { InputProps } from 'antd';
 import { Input } from 'antd';
 import type { FC } from 'react';
 
-export type FormInputProps = InputProps;
+export type FormInputProps = any;
 
 export const TextArea: FC<FormInputProps> = (props) => {
-  const { allowClear = true } = props;
-  return <Input.TextArea allowClear={allowClear} {...props} />;
+  return <Input.TextArea  {...props} />;
 };
